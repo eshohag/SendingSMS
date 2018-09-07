80e9c3b757976972c56bb42656a0c20acd53a273
MD. Shohag Mia <mdshohagmia14@outlook.com>
2018-09-07 13:09:41 +0600

    Initial

---
+++ SendingSMS/Controllers/SMSController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace SendingSMS.Controllers
+{
+    public class SMSController : Controller
+    {
+        // GET: SMS
+        public ActionResult Send()
+        {
+            // var accountSID = ConfigurationManager.AppSettings["TwilioSID"];
+            var accountSID = "AC29f489d11c6c661be530e2f5f1da34b8";
+
+            var authTocken = "c9aec7a4e04219880a2c6cc4bc453142";
+
+            TwilioClient.Init(accountSID, authTocken);
+            var toNumber = new PhoneNumber("+15344295519");
+            var fromNumber = new PhoneNumber("+18556293762");
+            var message = MessageResource.Create(
+                to: toNumber,
+                from: fromNumber,
+                body: "Hello Test SMS From Child Health Care Projects"
+            );
+            return Content(accountSID);
+        }
+    }
+}
